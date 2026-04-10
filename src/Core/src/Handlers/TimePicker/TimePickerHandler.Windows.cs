887745125143f5038b9a2acdaa9390c196888e59
Subhiksha Chandrasekaran <subhiksha.c@syncfusion.com>
2026-04-10 12:02:41 +0530

    Fix for timePicker windows

--- src/Core/src/Handlers/TimePicker/TimePickerHandler.Windows.cs
+++ src/Core/src/Handlers/TimePicker/TimePickerHandler.Windows.cs
@@ -1,20 +1,76 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.Maui.Platform;
 using WBrush = Microsoft.UI.Xaml.Media.Brush;
 
 namespace Microsoft.Maui.Handlers
 {
 	public partial class TimePickerHandler : ViewHandler<ITimePicker, TimePicker>
 	{
+		FlyoutBase? _flyout;
+
 		protected override TimePicker CreatePlatformView() => new TimePicker();
 
 		protected override void ConnectHandler(TimePicker platformView)
 		{
 			platformView.SelectedTimeChanged += OnSelectedTimeChanged;
+			platformView.Loaded += OnLoaded;
+
+			if (platformView.IsLoaded)
+				SubscribeFlyoutEvents(platformView);
 		}
 
 		protected override void DisconnectHandler(TimePicker platformView)
 		{
 			platformView.SelectedTimeChanged -= OnSelectedTimeChanged;
+			platformView.Loaded -= OnLoaded;
+			UnsubscribeFlyoutEvents();
+		}
+
+		void OnLoaded(object sender, RoutedEventArgs e)
+		{
+			if (PlatformView is not null)
+				SubscribeFlyoutEvents(PlatformView);
+		}
+
+		void SubscribeFlyoutEvents(TimePicker platformView)
+		{
+			UnsubscribeFlyoutEvents();
+
+			var flyoutButton = platformView.GetDescendantByName<Button>("FlyoutButton");
+			if (flyoutButton?.Flyout is FlyoutBase flyout)
+			{
+				_flyout = flyout;
+				_flyout.Opened += OnFlyoutOpened;
+				_flyout.Closed += OnFlyoutClosed;
+			}
+		}
+
+		void UnsubscribeFlyoutEvents()
+		{
+			if (_flyout is not null)
+			{
+				_flyout.Opened -= OnFlyoutOpened;
+				_flyout.Closed -= OnFlyoutClosed;
+				_flyout = null;
+			}
+		}
+
+		void OnFlyoutOpened(object? sender, object e)
+		{
+			if (VirtualView is null)
+				return;
+
+			VirtualView.IsOpen = true;
+		}
+
+		void OnFlyoutClosed(object? sender, object e)
+		{
+			if (VirtualView is null)
+				return;
+
+			VirtualView.IsOpen = false;
 		}
 
 		public static void MapFormat(ITimePickerHandler handler, ITimePicker timePicker)
